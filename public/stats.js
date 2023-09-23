6e6671b279505a681e8e83d34578054cb5782873
kh3dron <tristansaldanha@gmail.com>
2023-09-22 19:23:02 -0700

    async calls working

--- public/stats.js
+++ public/stats.js
@@ -20,14 +20,19 @@ function currentValFromSpeed(startTime, startValue, speedPerSecond) {
     return startValue + valueElapsed;
 }
 
-// Each stat has its own function
+// Functions with no API Calls
 
 function stat_kardashev_level() {
-    return 2.725;
+    startTime = new Date("2023-07-12");
+    startValue = 0.7276;
+    endTime = new Date("2060-1-1");
+    endValue = 0.7449;
+    val = currentValFromBounds(startTime, startValue, endTime, endValue)
+    return val.toFixed(11);
 }
 
-// https://www.worldometers.info/world-population/
 function stat_population_earth() {
+    // https://www.worldometers.info/world-population/
     startTime = new Date("2023-07-1");
     startValue = 8045311447;
     endTime = new Date("2025-07-1");
@@ -45,26 +50,37 @@ function stat_voyaver1_distance() {
     return val;
 }
 
-
-// implement http://api.open-notify.org/astros.json
-function stat_humansOffEarth() {
-    return 13;
+// Functions with API Calls
+async function get_humansOffEarth() {
+    try {
+        const response = await fetch('http://api.open-notify.org/astros.json');
+    
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        const data = await response.json();
+        const numberOfAstronauts = data.number;
+    
+        return numberOfAstronauts;
+      } catch (error) {
+        console.error('Error:', error);
+        throw error;
+      }
 }
+get_humansOffEarth().then((numberOfAstronauts) => {
+    document.getElementById("statD").innerText = numberOfAstronauts;
+}
+);
 
 function getRandomValue() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-
-
-
-
 function updateInternals() {
     document.getElementById("statA").innerText = stat_kardashev_level();
     document.getElementById("statB").innerText = stat_population_earth().toLocaleString();
     document.getElementById("statC").innerText = stat_voyaver1_distance().toLocaleString();
-    document.getElementById("statE").innerText = getRandomValue();
-    document.getElementById("statF").innerText = getRandomValue();
 }
 setInterval(updateInternals, 1000);
 
