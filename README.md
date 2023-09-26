287dc02f9b4d76bdf9446cb00911456a58381f5d
kh3dron <tristansaldanha@gmail.com>
2023-09-25 23:38:18 -0700

    stat: internet size

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # civilizational-dashboard
-test project for learning go 
+
+A simple dashboard for us to track core metrics for us as a species. 
+
+The figures on this site are not precise. For most cases, it's impossible to get even close to an accurate measurement. The point is to illustrate roughly the rate of change of these figures, and to track them within an order of magnitude. 
--- public/stats.js
+++ public/stats.js
@@ -29,7 +29,7 @@ function stat_kardashev_level() {
     endTime = new Date("2060-1-1");
     endValue = 0.7449;
     val = currentValFromBounds(startTime, startValue, endTime, endValue)
-    return val.toFixed(10);
+    return val.toFixed(12);
 }
 
 function stat_population_earth() {
@@ -78,17 +78,39 @@ function stat_top500_flops() {
     return 5239024665799999040258048n.toLocaleString();
 }
 
+function stat_top500_eflops() {
+    return (5239024665799999040258048n/1000000000000000000n).toLocaleString();
+}
+
 function stat_top500_power() {
     return 341029400n.toLocaleString();
 }
+
+function stat_internet_bits(){
+    // https://healthit.com.au/how-big-is-the-internet-and-how-do-we-measure-it/#:~:text=In%202020%2C%20the%20amount%20of,and%20consumed%20on%20the%20web.
+    startSize = 64e21;
+    startDate = new Date("2020-01-01");
+    endDate = new Date("2025-01-01");
+    endSize =175e21;
+    return 8*currentValFromBounds(startDate, startSize, endDate, endSize);
+}
+
+function stat_internet_terrabytes(){
+    startSize = 64e21;
+    startDate = new Date("2020-01-01");
+    endDate = new Date("2025-01-01");
+    endSize =175e21;
+    val = currentValFromBounds(startDate, startSize, endDate, endSize)/1e12;
+    // return int of val
+    return Math.round(val);
+}
+
+
 
 function getRandomValue() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-function pending(){
-    return "Pending";
-}
 
 function updateInternals() {
     document.getElementById("stat_kardashev").innerText = stat_kardashev_level();
@@ -96,11 +118,12 @@ function updateInternals() {
     document.getElementById("stat_furthest").innerText = stat_voyaver1_distance().toLocaleString();
     
     document.getElementById("stat_top500_flops").innerText = stat_top500_flops();
+    document.getElementById("stat_top500_eflops").innerText = stat_top500_eflops();
     document.getElementById("stat_top500_power").innerText = stat_top500_power();
-    
-    document.getElementById("pending").innerText = pending();
+    document.getElementById("stat_internet_bits").innerText = stat_internet_bits().toLocaleString();
+    document.getElementById("stat_internet_terrabytes").innerText = stat_internet_terrabytes().toLocaleString();
 }
-setInterval(updateInternals, 1000);
+setInterval(updateInternals, 10);
 
 
 updateInternals();
